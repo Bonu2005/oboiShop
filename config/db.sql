fa62370243bd0aa899bea7018021e5d7d970b189
ismoayeel <ergashboyevismoil922@gmail.comssh-keygen -o -t rsa -C windows-ssh@mcnz.com>
2025-02-04 10:23:53 +0500

    qwert

--- config/db.sql
+++ config/db.sql
@@ -7,4 +7,33 @@ CREATE Table user(
     phone VARCHAR(100),
     password VARCHAR(100),
     role ENUM("user","admin","superadmin") DEFAULT "user"
-)
+);
+
+CREATE TABLE category(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name_uz VARCHAR(255),
+    name_ru VARCHAR(255),
+    image VARCHAR(255)
+);
+
+CREATE TABLE brands(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name_uz VARCHAR(255),
+    name_ru VARCHAR(255),
+    image VARCHAR(255)
+);
+
+CREATE TABLE country(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name_uz VARCHAR(255),
+    name_ru VARCHAR(255),
+);
+
+CREATE TABLE categoryItem(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT,
+    product_id INT,
+    Foreign Key (category_id) REFERENCES category(id),
+    Foreign Key (product_id) REFERENCES product(id)
+);
+
